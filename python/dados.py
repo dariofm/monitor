3643fada8c83dbbb2ca2c96bf5fdcaaaab5d74e1
dariofm <36036935+dariofm@users.noreply.github.com>
2020-10-03 09:26:05 -0300

    upload python projeto

---
+++ python/dados.py
@@ -0,0 +1,50 @@
+import time, sys
+import os 
+from core.models import Uteis
+from core.models import Configuracoes
+import sqlite3
+import firebirdsql
+import configparser
+from funcoes import proximoNumero
+
+
+
+config = configparser.RawConfigParser()
+config.read('C:\\Kimmera Monitor\\monitor.ini')
+caixa = config.getint('TERMINAL', 'Caixa')
+
+
+conexao = firebirdsql.connect(user="SYSDBA",password="masterkey",database="C:\\Kimmera Monitor\\monitor.fdb",host="localhost",charset="ISO8859_1")
+
+cursor = conexao.cursor()
+uteis = Uteis()
+configuracoes = Configuracoes()
+database = uteis.conexao
+
+
+emitente = database["Pessoas"]
+for i in emitente.find({"_t.2":"Emitente"}):
+    nome = "'"+i["Nome"]+"'"
+    nomeFantasia = "'"+i["NomeFantasia"]+"'"
+    cnpj = "'"+i["Cnpj"]+"'"
+    endereco = "'"+i["Carteira"]["EnderecoPrincipal"]["Logradouro"]+"'"
+    numero = "'"+i["Carteira"]["EnderecoPrincipal"]["Numero"]+"'"
+    bairro = "'"+i["Carteira"]["EnderecoPrincipal"]["Bairro"]+"'"
+    cep = endereco = "'"+i["Carteira"]["EnderecoPrincipal"]["Cep"]+"'"
+    cidade = endereco = "'"+i["Carteira"]["EnderecoPrincipal"]["Municipio"]["Nome"]+"'"
+    
+    cursor.execute("update emitente set nome = %s,nomeFantasia = %s,cnpj = %s,endereco = %s,numero = %s,bairro = %s,cep = %s,cidade = %s"%(nome,nomeFantasia,cnpj,endereco,numero,bairro,cep,cidade))
+    conexao.commit()
+
+usuarios = database["Usuarios"]
+for i in usuarios.find({"Ativo":True,"$nor": [ { "_t.1":"UsuarioAdministrador"}]}):
+    nomeUsuario = "'"+i["Nome"]+"'"
+    
+    
+    cursor.execute("delete from usuario")
+    conexao.commit()    
+    idUsuario = proximoNumero("GEN_USUARIO_ID")
+    cursor.execute('insert into usuario (nome,id) values (%s,%s)'%(nomeUsuario,idUsuario))
+    conexao.commit()
+    
+conexao.close
---
+++ python/funcoes.py
@@ -0,0 +1,12 @@
+import firebirdsql
+
+
+conexao = firebirdsql.connect(user="SYSDBA",password="masterkey",database="C:\\Kimmera Monitor\\monitor.fdb",host="localhost",charset="ISO8859_1")
+cursor = conexao.cursor()
+
+def proximoNumero(GENERETOR):
+  cursor.execute('SELECT GEN_ID('+GENERETOR+',1) AS ID_ATUAL FROM RDB$DATABASE');
+  idAtual = 0
+  for i in cursor.fetchall():
+      idAtual = i[0]
+  return idAtual
