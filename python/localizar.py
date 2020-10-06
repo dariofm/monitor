825ce93053e364bb3bb3b270ef423d6b04d0251f
dariofm <36036935+dariofm@users.noreply.github.com>
2020-10-06 00:04:08 -0300

    função para localizar nota perdida

---
+++ python/localizar.py
@@ -0,0 +1,89 @@
+import time, sys
+import os 
+from core.models import Uteis
+from core.models import Configuracoes
+import sqlite3
+import firebirdsql
+import configparser
+from funcoes import proximoNumero
+from funcoes import trataData
+
+
+config = configparser.RawConfigParser()
+config.read('C:\\Kimmera Monitor\\monitor.ini')
+#caixa = config.getint('TERMINAL', 'Caixa')
+
+
+conexao = firebirdsql.connect(user="SYSDBA",password="masterkey",database="C:\\Kimmera Monitor\\monitor.fdb",host="localhost",charset="ISO8859_1")
+
+
+cursor = conexao.cursor()
+uteis = Uteis()
+configuracoes = Configuracoes()
+database = uteis.conexao
+
+
+#Inicialização das var
+
+produtos = []
+#Retorna produtos vendidos no Nota Manual 
+def retornaProdutos(nota_num):
+    movimento = database["Movimentacoes"]
+    produtos = []
+
+    iNumero = -1
+    for i in movimento.find({"Numero":nota_num}):
+        try:
+            for item in i["ItensBase"]:
+                iNumero = iNumero + 1
+                produtos.insert(0,
+                [i["ItensBase"][iNumero]["ProdutoServico"]["CodigoInterno"],
+                i["ItensBase"][iNumero]["ProdutoServico"]["Descricao"],
+                i["ItensBase"][iNumero]["ProdutoServico"]["UnidadeMedida"]["Sigla"],
+                i["ItensBase"][iNumero]["Quantidade"],
+                i["ItensBase"][iNumero]["PrecoUnitario"],
+                i["ItensBase"][iNumero]["Cancelado"]
+                ])
+        except:
+            produtos = ['Não há movimentação']
+    return produtos
+
+query = {"_t.3":"NotaFiscalManual","Numero":131}
+collection = {"Numero":1,"DataHoraEmissao":1,"NomeUsuario":1}
+
+movimento = database["Movimentacoes"]
+for i in movimento.find(query,collection).sort("Numero",-1).limit(1):
+    ultimo_numero = i["Numero"]
+    emissao = "'"+trataData(i["DataHoraEmissao"])+"'"
+    usuario = "'"+i["NomeUsuario"]+"'"
+ 
+    
+    
+    item = 0
+    
+    for i in retornaProdutos(ultimo_numero):
+        item = item + 1
+        idCodigo = proximoNumero('GEN_NOTA_MANUAL_ID')
+        codigoInterno = i[0]
+        descricao = "'"+i[1]+"'"
+        unidade = "'"+i[2]+"'"
+        quantidade = i[3]
+        valor_unitario = i[4]
+        total = i[3] * i[4]
+        canc = i[5]
+        impresso = 0
+        f = open('C:\\Kimmera Monitor\\log.txt', 'w')
+        try:
+            
+            f.write(str(valor_unitario)+' - '+str(retornaProdutos(ultimo_numero)))
+            
+            cursor.execute("insert into NOTA_MANUAL(id,numero,item,emissao,codigointerno,descricao,und,qtd,valor_unitario, valor_total,usuario,impresso) values (%s,%s,%s,%s,%s,%s,%s,%f,%f,%f,%s,%s) "%(idCodigo,ultimo_numero,item,emissao,codigoInterno,descricao,unidade,quantidade,valor_unitario,total,usuario,impresso))
+            conexao.commit()
+        except:
+            f.write(str(valor_unitario)+' - '+str(retornaProdutos(ultimo_numero)))
+        f.close()
+    #Atualiza último cupom gravado
+    cursor.execute('update param set controle = %s'%(ultimo_numero))
+    conexao.commit()
+
+
